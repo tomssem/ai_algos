826250817c5979130976f82b3b6a4d4809d8359e
Tom N <tfwnicholson+1@gmail.com>
2019-09-02 18:19:16 +0900

    Implement abstract baseclass for graph

--- graph_search/python/graph_search/graph.py
+++ graph_search/python/graph_search/graph.py
@@ -2,19 +2,73 @@
 Data structures for representing graphs
 """
 
-import copy
-from typing import List
+import abc
 
-class Vertex:
+class AbstractGraph(abc.ABC):
     """
-    Represents a vertex in an immutable graph
-    """
-    def __init__(self, children: List[Vertex]):
-        self._children = children
+    Abstract base class for all graph objects.
+    Nodes are represented by integers, and edges as tuples of `(vertex_from, vertex_to, weight)`,
+    where weight is a float representing the weight of the edge (this will be 1 in unweighted
+    graphs.
 
-    @property
-    def children(self):
+    Defines::
+     load
+     save
+     children_of
+     parents_of
+     edges_from
+     edges_to
+    """
+
+    @abc.abstractmethod
+    def save(self, path):
         """
-        Get the children of this node
+        Save a graph to the provided filepath, will overwrite any existing file. Returns concrete
+        graph object
+        :param path: the string path from to which to save the graph
+        :rtype: concrete class extending AbstractGraph
         """
-        return copy.deepcopy(self._children)
+
+    @abc.abstractclassmethod
+    def load(cls, path):
+        """
+        Read a graph from a file at the provided filepath. Returns concrete graph object
+        :param path: the string path from which to load a graph
+        :rtype: concrete class extending AbstractGraph
+        """
+
+    @abc.abstractmethod
+    def children_of(self, vertex):
+        """
+        Get children that are accessible to this node, along with accompanying weights
+        :param int vertex: the vertex we want to find all children of
+        :returns: A list of all children [(vertex, weight)]
+        :rtype: List[Tuple[int, float]]
+        """
+
+    @abc.abstractmethod
+    def parents_of(self, vertex):
+        """
+        Get parents that can access this node laong with accompanying weights.
+        :param int vertex: the vertex we want to find all parents of
+        :returns: A list of all parents [(vertex, weight)]
+        :rtype: List[Tuple[int, float]]
+        """
+
+    @abc.abstractmethod
+    def edges_from(self, vertex):
+        """
+        Get all edges that lead from supplied vertex
+        :param int vertex: the vertex we want to find all edges from
+        :returns: A list of all edges from this node [(vertex_out, vertex_in, weight)]
+        :rtype: List[Tuple[int, int, float]]
+        """
+
+    @abc.abstractmethod
+    def edges_to(self, vertex):
+        """
+        Get all edges that lead to supplied vertex
+        :param int vertex: the vertex we want to find all edges to
+        :returns: A list of all edges into this node [(vertex_out, vertex_in, weight)]
+        :rtype: List[Tuple[int, int, float]]
+        """
